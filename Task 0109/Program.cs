4ea0260602c3050134bed672b3ebde5c8935c7c3
Vitaliy <viurum986@mail.ru>
2022-04-11 22:25:21 +0300

    init Task 0109

---
+++ Task 0109/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N.
+
+int Promt(string message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine();
+    int Value = int.Parse(strValue);
+    return Value;
+}
+
+int m = Promt("Введите значение M => ");
+int n = Promt("Введите значение N => ");
+
+for (int i = m; i <= n; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write(" {0} ", i);
+    }
+}
+return 0;
+
+
